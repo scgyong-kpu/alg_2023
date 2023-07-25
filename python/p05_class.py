7a34c4f11f4f6d4fd61731adc98875ca3798aa94
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-07-25 18:44:27 +0900

    Python 05-2. Class: 객체는 속성을 런타임에 추가할 수 있다

--- python/p05_class.py
+++ python/p05_class.py
@@ -13,3 +13,14 @@ for flag in flags:
   todo = Hello if flag else func
   obj = todo() # 함수 호출과 객체 생성이 동일한 문법 구조에 의해 일어난다
   print('flag 값은:', flag, '이번에 얻은 것은:', obj)
+
+h1 = Hello()
+h2 = Hello()
+print('\n-- Python 의 class 객체는 임의의 속성(멤버변수) 을 런타임에 추가할 수 있다 --')
+h1.name = 'David'
+h2.age = 20
+for obj in h1, h2:
+  for attr in 'name', 'age':
+    has = '있다' if hasattr(obj, attr) else '없다'
+    print(f'객체 {obj} 는 속성 {attr} 이 {has}')
+
