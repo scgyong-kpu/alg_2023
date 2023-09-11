0ba95836b59de11c33c5a9f3be12969184092420
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-09-11 21:17:03 +0900

    Python 08-2. LC - List Comprehension 이용하기

--- python/p08_list_comprehension.py
+++ python/p08_list_comprehension.py
@@ -4,3 +4,8 @@ for i in range(10):
 
 print('-- 빈 배열을 준비하고 루프를 돌면서 초기화하기 --')
 print(f'{squares_1=}')
+
+squares_2 = [ i ** 2 for i in range(10) ]
+
+print('\n-- List Comprehension 이용하기 --')
+print(f'{squares_2=}')
