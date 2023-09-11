a7870111d396f1373c6a26344a65757853e398c6
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-09-11 22:45:19 +0900

    ch21bs-1. data_unsorted 의 100 개만 가지고 시작

--- src/ch2_1_bubble_sort.py
+++ src/ch2_1_bubble_sort.py
@@ -1,5 +1,5 @@
-# from data_unsorted import numbers
-from data_unsorted_a_lot import numbers
-numbers = numbers[:1000]
+from data_unsorted import numbers
+# from data_unsorted_a_lot import numbers
+# numbers = numbers[:1000]
 
 print(numbers)
