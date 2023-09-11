c04369b423834720482e353b82acb5bd748a1a47
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-09-11 23:59:59 +0900

    ch22ss-1. Selection Sort 의 한 바퀴. 최소값의 위치를 찾아 낸 다음 첫번째 원소와 교환

--- src/ch2_2_selection_sort.py
+++ src/ch2_2_selection_sort.py
@@ -1,13 +1,24 @@
 from data_unsorted import numbers
 # from data_unsorted_a_lot import numbers
+# from vis import SelectionSortVisualizer as Visualizer
+from vis import Dummy as Visualizer
+
 from random import randint, seed, shuffle
-from vis import SelectionSortVisualizer as Visualizer
-# from vis import Dummy as Visualizer
 from time import time
 
 def main():
   print('before:', array)
   count = len(array)
+
+  for a in range(1):
+    min_value = array[a]
+    min_at = a
+    for b in range(a + 1, count):
+      if min_value > array[b]:
+        min_value = array[b]
+        min_at = b
+    array[a], array[min_at] = array[min_at], array[a]
+    print(f'{min_at=}. swap {a} <=> {min_at}')
 
   print('after :', array)
 
