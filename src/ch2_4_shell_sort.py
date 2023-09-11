c38782c9cfc0f730b9ce627f8abb39aff606d392
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-09-12 08:21:14 +0900

    ch24sh-1. Level 1: gap 을 줄여가면서 insertion sort 수행. 기본 구현은 3중루프가 된다.

--- src/ch2_4_shell_sort.py
+++ src/ch2_4_shell_sort.py
@@ -6,10 +6,41 @@ from vis import ShellSortVisualizer as Visualizer
 from random import randint, seed, shuffle
 from time import time
 
+GAPS = [1750, 701, 301, 141, 63, 31, 15, 7, 3, 1, 0]
+def next_gap(gap):
+  for g in GAPS: 
+    if gap > g: return g
+
 def main():
   print('before:', array)
   count = len(array)
 
+  gap = next_gap(count / 2.5)
+  print(f'{count=} first_gap={gap}')
+  while True:
+    vis.set_gap(gap)
+    for offset in range(gap):
+      start = offset + gap
+      while start < count:
+        vis.mark_end(start, True)
+        v = array[start]
+        i = start
+        while i >= gap:
+          vis.compare(i-gap, i)
+          if array[i - gap] > v:
+            vis.shift(i-gap, i)
+            array[i] = array[i - gap]
+            vis.draw()
+            i -= gap
+          else:
+            break
+        vis.shift(start, i, True)
+        array[i] = v
+        vis.draw()
+        start += gap
+    gap = next_gap(gap)
+    if gap < 1: break
+    
   print('after :', array)
 
 if __name__ == '__main__':
