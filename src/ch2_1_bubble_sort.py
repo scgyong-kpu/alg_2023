f22d88b3a15aef6321bd7bdd328529e87fb98f31
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-09-11 23:14:05 +0900

    ch21bs-3. 랜덤하지만 항상 같은 결과가 나오게 (?)

--- src/ch2_1_bubble_sort.py
+++ src/ch2_1_bubble_sort.py
@@ -2,12 +2,13 @@ from data_unsorted import numbers
 # from data_unsorted_a_lot import numbers
 # numbers = numbers[:1000]
 
-from random import randint
+from random import randint, seed
 
 def main():
   print(array)
 
 if __name__ == '__main__':
+  seed('Hello') # 'Hello' 를 seed 로 고정하여 randint 가 항상 같은 결과가 나오게 한다
   count = randint(10, 30)
   array = numbers[:count]
   main()
