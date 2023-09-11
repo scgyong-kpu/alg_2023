6856de53dab3e02a8741f6c50508715e84afe51b
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-09-12 00:47:02 +0900

    ---------------- Ch6 - 3. Insertion Sort 시작 ----------------

---
+++ src/ch2_3_insertion_sort.py
@@ -0,0 +1,27 @@
+from data_unsorted import numbers
+# from data_unsorted_a_lot import numbers
+from random import randint, seed, shuffle
+from vis import InsertionSortVisualizer as Visualizer
+# from vis import Dummy as Visualizer
+from time import time
+
+def main():
+  print('before:', array)
+  count = len(array)
+
+  print('after :', array)
+
+if __name__ == '__main__':
+  seed('Hello') # 'Hello' 를 seed 로 고정하여 randint 가 항상 같은 결과가 나오게 한다
+  vis = Visualizer('Insertion Sort')
+
+  while True:
+    count = randint(10, 30)
+    array = numbers[:count]
+    vis.setup(vis.get_main_module())
+    main()
+    vis.draw()
+
+    again = vis.end()
+    if not again: break
+
