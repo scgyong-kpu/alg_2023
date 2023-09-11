8799503ec0ab06c4f459f9e41f5b946a1a7e80cd
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-09-11 23:12:54 +0900

    ch21bs-2. array 전역변수 설정하고 main 함수로 분리. 데이터의 크기를 랜덤하게.

--- src/ch2_1_bubble_sort.py
+++ src/ch2_1_bubble_sort.py
@@ -2,4 +2,12 @@ from data_unsorted import numbers
 # from data_unsorted_a_lot import numbers
 # numbers = numbers[:1000]
 
-print(numbers)
+from random import randint
+
+def main():
+  print(array)
+
+if __name__ == '__main__':
+  count = randint(10, 30)
+  array = numbers[:count]
+  main()
