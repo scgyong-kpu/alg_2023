8131917c5b10795b00971f76d718deb3a88bcd88
Kim, Ki-Yong <scgyong@tukorea.ac.kr>
2023-09-21 18:59:55 +0900

    04반 힌트.

--- src/ex1_1_sort_4.py
+++ src/ex1_1_sort_4.py
@@ -10,31 +10,46 @@ def bubble_sort(arr):
   print('-' * 60)
   print('Bubble Sort')
   print(f'before: {arr}')
+  count = len(arr)
+  for a in range(count - 1):
+    for b in range(count - 1 - a):
+      if ??: ???
+
   print(f'after : {arr}')
 
 def select_sort(arr):
   print('-' * 60)
   print('Selection Sort')
   print(f'before: {arr}')
+  while ???: # N 개의 최소값을 찾는다
+    while ???: # 최소값이 어디 있는지 알아낸다
+      pass
+    #바꾼다
+
   print(f'after : {arr}')
 
 def insert_sort(arr):
   print('-' * 60)
   print('Insertion Sort')
   print(f'before: {arr}')
+  while ???: # 1st loop: 1(두번째)부터 끝까지 주인공 시켜준다
+    while ???: # 2nd loop: 주인공을 가능한곳까지 왼쪽으로 보낸다
   print(f'after : {arr}')
 
 def shell_sort(arr):
   print('-' * 60)
   print('Shell Sort')
   print(f'before: {arr}')
+  while ???: # 1st loop: 갭을 점점 줄여서 1까지 돌린다
+    while ???: # 2nd loop: 갭부터 끝까지 주인공 시켜준다
+      while ???: # 주인공을 가능한곳까지 왼쪽으로 보낸다
   print(f'after : {arr}')
 
 def main():
   bubble_sort(array[:])
   insert_sort(array[:])
   select_sort(array[:])
-  shell_sort(array[:])
+  shell_sort(array[:25])
 
 if __name__ == '__main__':
   main()
