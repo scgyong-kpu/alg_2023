3cae35a3cf21d38337f26e1896d54446ef12103d
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-09-24 16:20:20 +0900

    ch27rl-5. numbers 를 쓰지 않고 랜덤한 4자리 숫자를 생성해본다.

--- src/ch2_7_radix_sort_lsd.py
+++ src/ch2_7_radix_sort_lsd.py
@@ -1,5 +1,5 @@
 # from data_unsorted import numbers
-from data_unsorted_a_lot import numbers
+# from data_unsorted_a_lot import numbers
 from vis import RadixSortLsdVisualizer as Visualizer
 # from vis import Dummy as Visualizer
 from time import time
@@ -57,11 +57,10 @@ if __name__ == '__main__':
   seed('HelloCountSort')
   vis = Visualizer('Radix Sort: LSD')
   while True:
-    kind = 1000                                # 0~999 사이의 수 생성
-    count = randint(30, 150)                   # 30개~150개 를 생성
+    kind = 10000                               # 0~9999사이의 수 생성
+    count = randint(30, 100)                   # 30개~100개 를 생성
     print(f'Creating data: {kind=} {count=}')
-    begin = 500 + randint(1, 1000)             # 시작위치 랜덤하게 결정
-    array = list(map(lambda x: x%kind, numbers[begin:begin+count]))
+    array = list(map(lambda x: randint(1, kind), range(count)))
     vis.setup(vis.get_main_module())
     main()
     vis.draw()
