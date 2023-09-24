5628331d27f177779027025ea962d4c238058b91
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-09-24 14:41:38 +0900

    ch25hs-4. Heapify 를 n/2 - 1 부터 0 까지 진행해 본다

--- src/ch2_5_heap_sort.py
+++ src/ch2_5_heap_sort.py
@@ -27,9 +27,10 @@ def main():
   vis.build_tree()
 
   last_parent_index = count // 2 - 1
-  for n in range(last_parent_index, last_parent_index - 2, -1):
+  for n in range(last_parent_index, -1, -1):
     vis.set_root(n)
     heapify(n, count)
+  vis.draw()
 
   print('after :', array)
 
