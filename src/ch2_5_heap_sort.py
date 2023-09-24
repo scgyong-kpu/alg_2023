be6fe9c516131c548720a6c8aa3a434f18891821
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-09-24 15:20:31 +0900

    nlogn vs n^1.5

--- src/ch2_5_heap_sort.py
+++ src/ch2_5_heap_sort.py
@@ -66,6 +66,9 @@ count=200000 elapsed=  1.599
 count=300000 elapsed=  2.575
 count=400000 elapsed=  3.371
 count=500000 elapsed=  4.321
+nlogn vs n^1.5
+y = 3xlog(2,x),  y=x^1.5 , x from 1 to 1000
+https://www.wolframalpha.com/input?i=y+%3D+3xlog%282%2Cx%29%2C++y%3Dx%5E1.5+%2C+x+from+1+to+1000
 '''
 
 if __name__ == '__main__':
