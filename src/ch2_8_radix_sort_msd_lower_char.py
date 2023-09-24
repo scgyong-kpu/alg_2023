ddd0287cb70565716be56924be7ec9838925c45f
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-09-24 17:23:02 +0900

    ch28rm-10. 단어에 사용된 글자수가 e~o 의 11글자뿐이라면 더 빠르게 정렬할 수 있다

--- src/ch2_8_radix_sort_msd_lower_char.py
+++ src/ch2_8_radix_sort_msd_lower_char.py
@@ -1,44 +1,43 @@
 words = [
-  'clean', 'prosy', 'rabbi', 'addle', 'smell', 'quite', 'lives', 'cross', 'synth', 'tweak', 
-  'medic', 'glade', 'breve', 'hotel', 'toing', 'scorn', 'tweet', 'zilch', 'react', 'fiche', 
-  'stunt', 'swing', 'merry', 'brine', 'miter', 'withy', 'brink', 'madly', 'spicy', 'cleft', 
-  'cheep', 'grief', 'links', 'write', 'fixed', 'yobbo', 'grave', 'cress', 'ensue', 'imply', 
-  'datum', 'quoit', 'twain', 'allow', 'kiosk', 'wheat', 'birch', 'flail', 'thong', 'bongo', 
-  'quilt', 'satyr', 'alibi', 'auger', 'event', 'scowl', 'shelf', 'melon', 'scrap', 'drupe', 
-  'nadir', 'kudos', 'pushy', 'bases', 'atone', 'knife', 'seise', 'odour', 'venom', 'filmy', 
-  'barre', 'davit', 'smart', 'canon', 'dixie', 'pease', 'adorn', 'liver', 'druid', 'baked', 
-  'tulip', 'unzip', 'nervy', 'hubby', 'sweat', 'scald', 'deity', 'nutty', 'mourn', 'unbar', 
-  'logic', 'stage', 'sisal', 'curvy', 'issue', 'pilot', 'wrote', 'quiff', 'willy', 'plate', 
-  'nobel', 'chubb', 'banjo', 'dirge', 'audit', 'rosin', 'scrag', 'papal', 'ducky', 'creed', 
-  'thief', 'pinko', 'hairy', 'cobra', 'waken', 'chime', 'cower', 'whelp', 'milky', 'fauna', 
-  'carob', 'locum', 'month', 'check', 'dicey', 'debit', 'women', 'calve', 'weepy', 'crash', 
-  'durex', 'ovoid', 'aside', 'blame', 'stela', 'humph', 'amino', 'smash', 'sorry', 'muzzy', 
-  'camel', 'sales', 'biddy', 'indue', 'jural', 'tress', 'egret', 'error', 'ogive', 'frizz', 
-  'quern', 'divvy', 'style', 'silky', 'elver', 'strum', 'sweep', 'taste', 'largo', 'there', 
-  'doric', 'slick', 'waive', 'rejig', 'comet', 'punch', 'frill', 'dolby', 'saver', 'brash', 
-  'sepoy', 'nippy', 'batty', 'unite', 'promo', 'towel', 'facia', 'digit', 'ripen', 'sylph', 
-  'softy', 'radio', 'aback', 'afore', 'sedan', 'ghyll', 'tying', 'mulch', 'caber', 'ghost', 
-  'sheaf', 'elfin', 'squad', 'patch', 'ester', 'roger', 'newel', 'tokay', 'hydro', 'tutti', 
-  'infer', 'widow', 'hives', 'inure', 'globe', 'cobol', 'annex', 'stout', 'ovary', 'wormy', 
-  'pique', 'empty', 'snowy', 'oxbow', 'cinch', 'plumy', 'piety', 'craft', 'fetch', 'mosey', 
-  'sense', 'tease', 'handy', 'gismo', 'ditty', 'coypu', 'lunch', 'screw', 'pager', 'ferry', 
-  'kinky', 'owlet', 'guess', 'whisk', 'epoch', 'coney', 'flour', 'spout', 'awoke', 'gnash', 
-  'dinky', 'manic', 'nacho', 'rerun', 'serge', 'flier', 'waltz', 'pubis', 'shove', 'bossa', 
-  'swath', 'mousy', 'pivot', 'frock', 'muzak', 'bogie', 'tenet', 'impel', 'ahead', 'stuck', 
-  'rural', 'judge', 'pommy', 'papaw', 'senna', 'testa', 'slash', 'unity', 'roost', 'dhoti', 
-  'solid', 'dicky', 'crier', 'seven', 'stick', 'curse', 'haiku', 'laver', 'amuse', 'store', 
-  'union', 'build', 'knurl', 'rough', 'anion', 'nappy', 'solve', 'halal', 'assay', 'stile', 
-  'bunny', 'ember', 'baron', 'sauce', 'boost', 'spoof', 'ledge', 'jolly', 'swift', 'krone', 
-  'fecal', 'badge', 'fusty', 'tumid', 'leger', 'waver', 'folly', 'islam', 'squid', 'fuzzy', 
-  'hammy', 'ducks', 'tongs', 'final', 'foray', 'femme', 'groan', 'vapid', 'iliac', 'crest', 
-  'skull', 'drive', 'melba', 'dingy', 'wrack', 'emery', 'lucky', 'savoy', 'trawl', 'zebra', 
-  'tepid', 'thick', 'objet', 'gouda', 'sleep', 'downy', 'twine', 'penal', 'owner', 'tangy', 
+  'mog', 'jim', 'km', 'lining', 'mingle', 'ell', 'folk', 'melon', 'ln', 'link', 
+  'knife', 'fennel', 'loon', 'john', 'ff', 'felloe', 'liking', 'lino', 'om', 'keg', 
+  'joke', 'no', 'hog', 'jell', 'fino', 'elfin', 'gin', 'lone', 'oh', 'gong', 
+  'ogee', 'oi', 'jig', 'filling', 'g', 'ge', 'mn', 'femme', 'fen', 'kj', 
+  'gene', 'online', 'mg', 'goggle', 'emf', 'loll', 'meek', 'l', 'gem', 'filing', 
+  'infill', 'hello', 'ink', 'monk', 'kg', 'ghillie', 'elf', 'gm', 'leo', 'genie', 
+  'hoe', 'he', 'eke', 'moll', 'gnomon', 'fm', 'lei', 'million', 'going', 'feminine', 
+  'infilling', 'liege', 'mo', 'o', 'goon', 'hg', 'legging', 'holm', 'enjoin', 'e', 
+  'mini', 'logging', 'kin', 'hen', 'logo', 'flee', 'inf', 'fog', 'knee', 'limn', 
+  'jingo', 'lf', 'log', 'non', 'lj', 'goo', 'hmg', 'joe', 'knell', 'minim', 
+  'menfolk', 'feel', 'mlle', 'ken', 'home', 'if', 'linen', 'ne', 'lo', 'knoll', 
+  'mime', 'ooh', 'nomen', 'hill', 'kohl', 'efl', 'offline', 'lee', 'king', 'legion', 
+  'one', 'hike', 'genome', 'neigh', 'lifelong', 'him', 'gigolo', 'fo', 'fe', 'ego', 
+  'mom', 'hi', 'fine', 'ni', 'ongoing', 'ho', 'noh', 'jog', 'lion', 'loin', 
+  'fill', 'm', 'nee', 'join', 'noggin', 'neon', 'none', 'hm', 'gi', 'hook', 
+  'ion', 'in', 'gen', 'mono', 'feign', 'look', 'hinge', 'moonie', 'nil', 'ginkgo', 
+  'kilo', 'fief', 'ikon', 'moon', 'long', 'hf', 'lifeline', 'lingo', 'mink', 'nigh', 
+  'k', 'killing', 'hoi', 'mongol', 'nikkei', 'ime', 'hoof', 'me', 'floe', 'omen', 
+  'jiff', 'mike', 'foe', 'ingoing', 'leg', 'kiln', 'fin', 'noel', 'niff', 'minion', 
+  'gnome', 'ill', 'ogle', 'info', 'igloo', 'fife', 'milk', 'loo', 'en', 'high', 
+  'f', 'kook', 'miff', 'hele', 'hmi', 'longing', 'kink', 'n', 'fee', 'mm', 
+  'hole', 'hellene', 'mien', 'moo', 'homing', 'jiggle', 'inkling', 'll', 'off', 'goof', 
+  'lifelike', 'feline', 'lie', 'jingle', 'eel', 'filo', 'nook', 'eeg', 'gillie', 'leonine', 
+  'ko', 'elk', 'honk', 'lien', 'mme', 'feeling', 'kneel', 'fleming', 'em', 'glee', 
+  'mil', 'homo', 'offing', 'engine', 'limekiln', 'film', 'giggle', 'folio', 'ming', 'men', 
+  'gone', 'oho', 'hone', 'lemming', 'iom', 'line', 'elm', 'liken', 'mile', 'henge', 
+  'leek', 'golf', 'gloom', 'eon', 'kill', 'kimono', 'ok', 'inglenook', 'ilk', 'limo', 
+  'lilo', 'ohm', 'melee', 'imf', 'hh', 'milfoil', 'kl', 'megohm', 'molehill', 'gel', 
+  'kennel', 'ml', 'noon', 'mf', 'inn', 'felon', 'ghee', 'helm', 'keel', 'memo', 
+  'nne', 'jinnee', 'on', 'hemline', 'nine', 'joie', 'glen', 'of', 'niggle', 'oil', 
+  'nom', 'mill', 'nominee', 'fling', 'hell', 'lego', 'gemini', 'finn', 'eh', 'fie', 
+  'onion', 'lemon', 'li', 'life', 'flog', 'jink', 'joggle', 'ofghijklmno', 'ofghijklmni', 'ofghijklmok'
 ]
 
-BASE = ord('a') - 1 # ascii code of 'a' = 97
+# 단어에 사용된 글자는 e~o 의 11글자 뿐이다
+BASE = ord('e') - 1 # ascii code of 'e' = 101
 
 def radix_lower_char_msd(array, left, right, depth=0):
-  counts = [0 for _ in range(27)] # 알파벳 'a' 이면 1, 'z' 면 26. 없으면 0. 총 27가지.
+  counts = [0 for _ in range(12)] # 알파벳 'e' 이면 1, 'o' 면 11. 없으면 0. 총 12가지.
   for i in range(left, right+1):
     string = array[i]
     str_len = len(string)
@@ -48,7 +47,7 @@ def radix_lower_char_msd(array, left, right, depth=0):
     counts[slot] += 1
 
   # print(' ' * depth, f'{counts=}')
-  for i in range(26):
+  for i in range(11):
     counts[i+1] += counts[i]
   # print(' ' * depth, f'index={counts}')
 
@@ -66,9 +65,9 @@ def radix_lower_char_msd(array, left, right, depth=0):
   if depth > 0: print(' ' * depth, f'{depth=} {left=}, {right=}', array[left:right+1])
   if depth > 0: print(' ' * depth, '-' * 30)
 
-  for i in range(27):
+  for i in range(12):
     sub_l = left + counts[i]
-    sub_r = left + counts[i+1] - 1 if i < 26 else right
+    sub_r = left + counts[i+1] - 1 if i < 11 else right
     # char = chr(i+BASE) if i > 0 else ' '
     # needs_recursion = 'Needs Recursion' if sub_l < sub_r else '-'
     # sub_arr = array[sub_l:sub_r+1]
