02491f49a65b82aee97f6ed5f1b035284a55abe4
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-09-24 14:33:24 +0900

    ---------------- Ch6 - 5. Heap Sort 시작 ----------------

---
+++ src/ch2_5_heap_sort.py
@@ -0,0 +1,26 @@
+from data_unsorted import numbers
+# from data_unsorted_a_lot import numbers
+from random import randint, seed, shuffle
+from vis import HeapSortVisualizer as Visualizer
+# from vis import Dummy as Visualizer
+from time import time
+
+def main():
+  print('before:', array)
+  count = len(array)
+  print('after :', array)
+  vis.draw()
+
+
+if __name__ == '__main__':
+  seed('Hello')
+  vis = Visualizer('Heap Sort')
+  while True:
+    array = numbers[:randint(10, 30)]
+    vis.setup(vis.get_main_module())
+    startedOn = time()
+    main()
+    elapsed = time() - startedOn
+    print(f'Elapsed time = {elapsed:.3f}s')
+    again = vis.end()
+    if not again: break
