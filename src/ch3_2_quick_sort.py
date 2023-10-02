afc87490fdca1127003c5e6d18d91685707e87dd
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-10-03 04:19:37 +0900

    ch32qs-2. 왼쪽에서 pivot 값보다 큰 값을, 오른쪽에서 pivot 값보다 작은 값을 찾는다

--- src/ch3_2_quick_sort.py
+++ src/ch3_2_quick_sort.py
@@ -9,7 +9,42 @@ def main():
   print('before:', array)
   count = len(array)
 
+  vis.push(0, count-1)
+  partition(0, count-1)
+
   print('after :', array)
+
+def partition(left, right):
+
+  pi = left               # pi = Pivot Index
+  pivot = array[pi]       # pivot = value
+
+  p, q = left, right + 1  # 후보선수들 출전준비
+
+  while True:             # p 와 q 가 역전할때까지
+    while True:           # 왼쪽에서 pivot 보다 큰 값을 찾을때까지
+      p += 1
+      vis.set_p(p)
+      if q < p: break
+      if p <= right: vis.compare(pi, p)
+      if p > right or array[p] >= pivot: break 
+      # 왼쪽에서 pivot 보다 큰 값을 찾았다
+
+      if p <= right: vis.set_left(p)
+
+    while True:           # 오른쪽에서 pivot 보다 작은 값을 찾을때까지
+      q -= 1
+      vis.set_q(q)
+      if q < p: break
+      if q >= left: vis.compare(pi, q)
+      if q < left or array[q] <= pivot: break
+      # 오른쪽에서 pivot 보다 작은 값을 찾았다
+
+      if q >= left: vis.set_right(q)
+
+    vis.set_left(p)
+    vis.set_right(q)
+    break
 
 if __name__ == '__main__':
   seed('Hello')
