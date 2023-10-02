755c6ce16b103160e1bfa866c1ce93cdecb11208
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-10-03 05:05:58 +0900

    ch33se-2. selection() 을 함수로 분리.

--- src/ch3_3_selection.py
+++ src/ch3_3_selection.py
@@ -12,8 +12,12 @@ def main():
   qs.vis = vis
   print('before:', array)
   count = len(array)
-  vis.push(0, count-1, n_th)
-  pi = qs.partition(0, count-1)
+  value = selection(0, count-1, n_th)
+
+def selection(left, right, k): #right=inclusive
+  print(f'{left=} {right=} {k=}')
+  vis.push(left, right, k)
+  pi = qs.partition(left, right)
   vis.set_pivot(pi)
 
 if __name__ == '__main__':
