fffb42b53edf3703238222fe932319cd7fec7834
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-10-03 04:16:42 +0900

    Revert "ch32qs-1. 가장 왼쪽의 값을 pivot 으로 삼아서 pivot 보다 작은 수는 왼쪽으로, 큰 수는 오른쪽으로 몰아준다."
    This reverts commit 3e8ce70b36aba9342df22af7eed2154c0b6ba90c.

--- src/ch3_2_quick_sort.py
+++ src/ch3_2_quick_sort.py
@@ -8,53 +8,8 @@ from random import randint, seed, shuffle
 def main():
   print('before:', array)
   count = len(array)
-  vis.push(0, count-1)
-  pivot = partition(0, count-1)
-  vis.set_pivot(pivot)
+
   print('after :', array)
-
-def partition(left, right):
-
-  pi = left               # pi = Pivot Index
-  pivot = array[pi]       # pivot = value
-
-  p, q = left, right + 1  # 후보선수들 출전준비
-
-  while True:             # p 와 q 가 역전할때까지
-    while True:           # 왼쪽에서 pivot 보다 큰 값을 찾을때까지
-      p += 1
-      vis.set_p(p)
-      if q < p: break
-      if p <= right: vis.compare(pi, p)
-      if p > right or array[p] >= pivot: break 
-      # 왼쪽에서 pivot 보다 큰 값을 찾았다
-
-      if p <= right: vis.set_left(p)
-
-    while True:           # 오른쪽에서 pivot 보다 작은 값을 찾을때까지
-      q -= 1
-      vis.set_q(q)
-      if q < p: break
-      if q >= left: vis.compare(pi, q)
-      if q < left or array[q] <= pivot: break
-      # 오른쪽에서 pivot 보다 작은 값을 찾았다
-
-      if q >= left: vis.set_right(q)
-
-    if p >= q: break      # p 와 q 가 만났다면 더이상 swap 은 없다
-
-    vis.set_left(p)
-    vis.set_right(q)
-    vis.swap(p, q)
-
-    array[p], array[q] = array[q], array[p] 
-    # p 는 pivot 보다 큰 값의 위치, q 는 pivot 보다 작은 값의 위치이므로 둘을 바꾼다
-  
-  if left != q: # pivot 값보다 작은 것이 하나도 없다면 left 는 q 와 같다.
-    vis.swap(left, q, True)
-    array[left], array[q] = array[q], array[left] # pivot 값의 위치를 확정시킨다
-
-  return q  # 결정된 pivot 의 위치를 리턴한다
 
 if __name__ == '__main__':
   seed('Hello')
