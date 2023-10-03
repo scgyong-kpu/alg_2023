fa59bb2c940f80994bfd5627740b1036f4c4087e
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-10-03 17:49:32 +0900

    ch34cp-4. D&C 시작: 1개일때는 거리가 없고 2개일때는 바로 해이고 3개일때는 brute_force 를 쓴다.

--- src/ch3_4_closest_pair.py
+++ src/ch3_4_closest_pair.py
@@ -32,10 +32,36 @@ def brute_all():
   # vis.pop()
   return s,e,d
 
+def devide_and_conquer():
+  cities.sort(key=lambda c:c.x) # x 좌표를 기준으로 모든 도시들을 정렬해둔다
+  for i in range(len(cities)):  # 정렬된 모든 도시들에 대하여
+    cities[i].index = i         # 번호를 매겨 둔다
+  global y_aligned
+  y_aligned = sorted(cities, key=lambda c:c.y) # y 좌표를 기준으로 정렬된 도시 배열을 만들어 둔다
+
+  s,e,d = closest_pair(cities, 0, len(cities) - 1)
+  return s,e,d
+
+# 함수 인자: 배열, 시작인덱스, 끝인덱스(inclusive)
+# 함수 리턴: 가장 가까운 도시들의 - 출발인덱스, 도착인덱스, 거리
+def closest_pair(arr, left, right):
+  size = right - left + 1
+  if size <= 1:             # 도시가 1개라면
+    return -1, -1, 0        # "거리" 자체가 성립하지 않는다.
+  if size == 2:                                             # 도시가 2개라면
+    s,e,d = left, right, distance(arr[left], arr[right])    # 그 두 도시가 가장 가깝다
+    vis.set_closest(left, right, s, e, d)
+    return s,e,d
+  if size == 3:                                      # 도시가 3개라면
+    vis.set_phase('Brute force between 3 cities')    # brute force 를 활용한다
+    s,e,d = brute_force(arr, left, right)
+    vis.set_closest(left, right, s, e, d)
+    return s,e,d
+
 def main():
   print(cities)
-  s,e,d = brute_all()
-  # s,e,d = devide_and_conquer()
+  # s,e,d = brute_all()
+  s,e,d = devide_and_conquer()
   # print(s,e,d)
   print(cities[s],cities[e],d)
 
@@ -44,7 +70,7 @@ if __name__ == '__main__':
   vis = Visualizer('Closest Pair')
   while True:
     beg = randint(0, 100)
-    end = randint(beg+10, beg+20)
+    end = beg+3 #randint(beg+10, beg+20) # 일단 3개만 해본다
     cities = five_letter_cities[beg:end]
     vis.setup(vis.get_main_module())
     main()
