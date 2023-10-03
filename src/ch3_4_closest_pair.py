b8b5531b09fcdbf2381017eabd5f7191644c056a
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-10-03 17:18:23 +0900

    ch34cp-1. brute force 2 개 도시

--- src/ch3_4_closest_pair.py
+++ src/ch3_4_closest_pair.py
@@ -4,12 +4,40 @@ from data_city import City, five_letter_cities
 from random import randint, seed, shuffle
 from math import sqrt
 
+def distance_sq(c1, c2):
+  dx, dy = c1.x - c2.x, c1.y - c2.y
+  return dx ** 2 + dy ** 2
+
+def distance(c1, c2):
+  dx, dy = c1.x - c2.x, c1.y - c2.y
+  return sqrt(dx ** 2 + dy ** 2)
+
+def brute_force(arr, left, right):
+  n_cities = len(cities)
+  closest = [-1, -1, float('inf')]
+  for i1 in range(left, right+1):
+    c1 = cities[i1]
+    for i2 in range(i1+1, right+1):
+      c2 = cities[i2]
+      dist = distance(c1, c2)
+      vis.compare(i1,i2,dist)
+      if dist < closest[2]:
+        closest = [i1, i2, dist]
+  return closest
+
+def brute_all():
+  n_cities = len(cities)
+  vis.push()
+  s,e,d = brute_force(cities, 0, n_cities - 1)
+  # vis.pop()
+  return s,e,d
+
 def main():
   print(cities)
-  # s,e,d = brute_all()
+  s,e,d = brute_all()
   # s,e,d = devide_and_conquer()
   # print(s,e,d)
-  # print(cities[s],cities[e],d)
+  print(cities[s],cities[e],d)
 
 if __name__ == '__main__':
   seed('Closest')
