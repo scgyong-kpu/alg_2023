07ae5e5c964c782a3a5b2cc3f0c54feaa637ffd6
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-10-24 00:59:25 +0900

    ch40city-7. next() 구조 적용

--- src/ch4_1_mst_kruskal.py
+++ src/ch4_1_mst_kruskal.py
@@ -3,7 +3,11 @@ import data_sample_cities as dsc
 
 if __name__ == '__main__':
   vis = Visualizer('Cities')
-  cities, edges = dsc.cities, dsc.edges
-  vis.setup(vis.get_main_module())
-  vis.draw()
-  vis.end()
+  while True:
+    cities, edges = dsc.cities, dsc.edges
+    vis.setup(vis.get_main_module())
+    vis.draw()
+    again = vis.end()
+    if not again: break
+    dsc.next()
+
