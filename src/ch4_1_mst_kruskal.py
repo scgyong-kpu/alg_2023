11ed88331f32c76af0f3c8de779f9c31329c69ff
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-10-24 01:11:21 +0900

    ch40city-10. Kruskal Visualizer 적용

--- src/ch4_1_mst_kruskal.py
+++ src/ch4_1_mst_kruskal.py
@@ -1,8 +1,8 @@
-from vis import PlanarVisualizer as Visualizer
+from vis import KruskalVisualizer as Visualizer
 import data_sample_cities as dsc
 
 if __name__ == '__main__':
-  vis = Visualizer('Cities')
+  vis = Visualizer('MST - Kruskal')
   while True:
     cities, edges = dsc.cities, dsc.edges
     vis.setup(vis.get_main_module())
