fad6ced8b6e3df4aab9f4865d903eead6e8a24e4
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-10-31 02:22:12 +0900

    ch45hu-3. Node 를 구성하고 정렬

--- src/ch4_5_huffman.py
+++ src/ch4_5_huffman.py
@@ -9,3 +9,34 @@ for ch in contents:
     counts[ch] += 1
 
 print(counts)
+
+class Node:
+  def __init__(self, freq, ch, left=None, right=None, code=None):
+    self.freq = freq
+    if ch == '\n': ch = '\\n'
+    elif ch == '\\': ch = '\\\\'
+    self.ch = ch
+    self.left = left
+    self.right = right
+    self.code = code
+  def __repr__(self):
+    s = f'<{self.freq}/{self.ch}'
+    if self.left is not None:
+      s += ' L={self.left}'
+    if self.right is not None:
+      s += ' R={self.right}'
+    s += '>'
+    return s
+  def __lt__(self, other): # 객체를 빈도수로 비교하기위해
+    return self.freq < other.freq
+
+nodes = []
+for ch in counts:
+  node = Node(counts[ch], ch)
+  nodes.append(node)
+
+print('----- Before sort -----')
+print(nodes)
+nodes.sort()
+print('----- After  sort -----')
+print(nodes)
