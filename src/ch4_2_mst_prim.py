8e0558aadcdee5b6a565f1778653bcba3998d451
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-10-30 10:31:59 +0900

    ch42pr-6. 알려진 점까지의 가중치를 weights 에 저장해 두기로 한다. 시작점의 weight 를 0 으로 초기화하고 시작한다.

--- src/ch4_2_mst_prim.py
+++ src/ch4_2_mst_prim.py
@@ -22,8 +22,15 @@ def print_adj_matrix():
 def main():
   global n_cities
   n_cities = len(cities)
+
   build_graph()
 
+  start_city_index = 0
+  print(f'{n_cities} cities, starts from {cities[start_city_index]}')
+
+  global weights
+  weights = []
+  weights.append((0, start_city_index))
 
 if __name__ == '__main__':
   vis = Visualizer('Minimum Spanning Tree - Prim')
