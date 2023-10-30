532cb0683a9e0a6ec4c84c31e67a9c976ee83bb0
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-10-30 11:17:52 +0900

    array 에서 가장 작은 weight 찾기

--- src/ch4_2_mst_prim.py
+++ src/ch4_2_mst_prim.py
@@ -33,6 +33,26 @@ def main():
   weights[start_city_index] = 0
   print(weights)
 
+  global mst
+  mst = []
+  while weights:
+    print('<', weights)
+    w, ci = pop_smallest_weight()
+    print('>', weights)
+
+    if len(mst) <= 1: break
+
+def pop_smallest_weight():
+  min_wi = -1
+  min_w = float('inf')
+  for wi in range(n_cities):
+    w = weights[wi]
+    if w < min_w:
+      min_w = w
+      min_wi = wi
+  if min_wi >= 0: weights[min_wi] = float('inf')
+  return min_wi, min_w
+
 if __name__ == '__main__':
   vis = Visualizer('Minimum Spanning Tree - Prim')
   idx = 0
