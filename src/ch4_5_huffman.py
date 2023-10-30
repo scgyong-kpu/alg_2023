f1216fa5cd865fa742cecdfb9f6fc9e559d9abab
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-10-31 02:03:52 +0900

    ch45hu-2. dict() 대신 defaultdict() 사용

--- src/ch4_5_huffman.py
+++ src/ch4_5_huffman.py
@@ -1,12 +1,11 @@
+from collections import defaultdict
+
 with open('data_knapsack.json') as f:
   contents = f.read()
   print(f'{len(contents)=}')
 
-counts = dict()
+counts = defaultdict(int)
 for ch in contents:
-  if ch in counts:
     counts[ch] += 1
-  else:
-    counts[ch] = 1
 
 print(counts)
