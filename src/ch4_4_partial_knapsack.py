93ef28c06ed9b180edb0f522f0be901f137af3df
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-10-31 01:45:12 +0900

    ch44pn-2. 차례대로 출력

--- src/ch4_4_partial_knapsack.py
+++ src/ch4_4_partial_knapsack.py
@@ -5,3 +5,10 @@ print(f'Before: {mats=}')
 mats.sort(key=lambda m:m['price']/m['weight'], reverse=True)
 print(f'After : {mats=}')
 
+cap = 85
+items = []
+weight_sum = 0
+price_sum = 0
+
+for mat in mats:
+  print(f'{mat=}')
