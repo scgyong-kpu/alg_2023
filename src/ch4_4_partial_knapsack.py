a0f32402740a84ce16a696f670c956d391ac1057
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-10-31 01:44:10 +0900

    ch44pn-1. 단위당 무게로 정렬

--- src/ch4_4_partial_knapsack.py
+++ src/ch4_4_partial_knapsack.py
@@ -1,4 +1,7 @@
 import json
 with open('data_knapsack.json') as f:
   mats = json.load(f)
-print(f'{mats=}')
+print(f'Before: {mats=}')
+mats.sort(key=lambda m:m['price']/m['weight'], reverse=True)
+print(f'After : {mats=}')
+
