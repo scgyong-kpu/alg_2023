91a14feeb29f9280cdc21b0ee8e1593e93135d5e
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-11-04 22:48:11 +0900

    ch46sc-7. r 을 눌러 랜덤한 데이터로 진행할 수 있도록 한다

--- src/ch4_6_set_cover.py
+++ src/ch4_6_set_cover.py
@@ -1,5 +1,6 @@
 from vis import SetCoverVisualizer as Visualizer
 from copy import deepcopy
+from random import randint, seed
 
 # Set Cover 시작
 u={1,2,3,4,5,6,7,8,9,10}
@@ -39,11 +40,7 @@ def main():
     C.append(S)      # 결과 배열인 C 에 넣는다
     print(f'{C=}')
 
-print(u)              # u 의 원소를 모두 출력
-print(f[1], f[5])     # f[1] 과 f[5] 를 각각 출력
-print(f[1] | f[5])    # f[1] 과 f[5] 의 합집합을 출력해 보면 u 와 같다
-# 따라서 위 두 집합을 선택하면 최소 선택으로 u 를 모두 cover 할 수 있다
-
+seed('SetCover')
 vis = Visualizer('Set Cover - Simple Set')
 while True:
   U = deepcopy(u) # 원본에서 제거하면서 진행할 것이므로
@@ -54,3 +51,23 @@ while True:
   vis.draw()
   again = vis.end()
   if not again: break
+
+  n_cities = randint(15, 30)
+  print(f'{n_cities=}')
+  u = set()
+  f = []
+  si = 0
+  while True:
+    s = set()
+    sc = randint(2, 12)
+    for i in range(sc):
+      ci = (si + randint(1, 12)) % n_cities
+      s.add(ci)
+    u |= s
+    f.append(s)
+    # print(s)
+    if len(u) == n_cities: break
+
+    si = (si + 1) % n_cities
+
+
