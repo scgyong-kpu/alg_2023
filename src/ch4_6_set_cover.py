5ef1a6dcfa77c0b49d3442aacbb893b0e76e5d74
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-11-04 22:39:36 +0900

    ch46sc-3. 어디에서 가장 많았는지를 기억해 둔다

--- src/ch4_6_set_cover.py
+++ src/ch4_6_set_cover.py
@@ -24,6 +24,7 @@ vis.reset()
 
 # 나중에 Loop 로 변경하기 위해 강제 들여쓰기 목적으로 if True 를 쓴다
 if True:    
+    max_i, max_c = -1, 0
     for i in range(len(F)):        # subset 들을 대상으로 Loop 를 돈다
       cnt = 0
       vis.comp(i)
@@ -31,7 +32,10 @@ if True:
       for v in subset:             # 그냥 for v in F[i] 로 해도 좋다
         if v in U: cnt += 1        # U 에 원소가 있으면 cnt 를 증가한다
         vis.comp(i, v, cnt)
-    vis.fix(3)                     # 임시로 3 번이 가장 많은 원소가 겹침을 보인다
+      if max_c < cnt:              # 겹치는 갯수 중 max 를 max_c,
+        max_i, max_c = i, cnt      # 그 위치를 max_i 에 저장한다 
+    print(f'{max_i=}')
+    vis.fix(max_i)                 # max_i 번째에 가장 원소가 많이 겹친다
 
 print(u)              # u 의 원소를 모두 출력
 print(f[1], f[5])     # f[1] 과 f[5] 를 각각 출력
