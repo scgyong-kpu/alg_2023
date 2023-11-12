bb6526563bc4b412d3f1f81c03aab7d3ea87c42b
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-11-13 00:34:54 +0900

    ch53cm-2. 부분문제가 2 일 때의 곰셈수를 계산한다

--- src/ch5_3_chained_matrix_mult.py
+++ src/ch5_3_chained_matrix_mult.py
@@ -14,7 +14,23 @@ class ChainedMatrixMult:
     self.P = [[ 0 for _ in range(self.matrix_count + 1) ] for _ in range(self.matrix_count + 1) ]
 
   def main(self):
-    vis.start_all_candidates()
+    vis.start()
+    count = self.matrix_count                # 행열은 모두 count 개 이다
+    for sub_mult_count in range(2, 2 + 1):   # 일단 부분분제의 크기가 2개 일 때만 해 본다
+      vis.sub(sub_mult_count)
+      max_start = count - sub_mult_count + 1 # 부분문제 크기가 커지면 시작위치가 제한된다
+      for start in range(1, max_start + 1):  # 부분문제의 시작위치를 변경시키며 계산한다
+        end = start + sub_mult_count - 1 # inclusive end
+        vis.range(start, end)
+        self.C[start][end] = float('inf')    # start~end 까지의 횟수를 inf 로 초기화한다
+        for k in range(start, end):
+          temp = self.C[start][k] + self.C[k+1][end] + self.sizes[start-1]*self.sizes[k]*self.sizes[end]
+          # 곱셈을 해당 부분에 넣었을 때 이득이 있는지 확인한다
+          vis.compare(k, self.C[start][end] > temp)
+          if self.C[start][end] > temp:
+            self.C[start][end] = temp        # start~end 까지의 횟수가 몇번인지 저장한다
+            self.P[start][end] = k           # 마지막 곱셈을 어디서 해야 하는지 저장한다
+            vis.update()
 
 if __name__ == '__main__':
   # seed('Hello')
