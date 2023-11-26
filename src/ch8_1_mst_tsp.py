25e875626e1bdbb6f2f8630edad147fd687217ab
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-11-26 14:17:38 +0900

    ch81mt-3. MST - Prim : 두번째 점을 확정하며 주위 점들에 대해 정보를 갱신한다

--- src/ch8_1_mst_tsp.py
+++ src/ch8_1_mst_tsp.py
@@ -47,7 +47,7 @@ class TspMst:
         self.weights[adj] = weight           # adj 까지 가는 가까운 거리는 weight 이며
         self.origins[adj] = v                # adj 까지 가려면 v 를 통해서 가야 한다
 
-      if len(self.completed) == 1: break
+      if len(self.completed) == 2: break
 
     vis.finish()
 
