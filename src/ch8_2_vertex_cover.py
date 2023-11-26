52c7670a327c46b27b33fcdb31b93e9df8435a44
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-11-26 16:43:02 +0900

    ch82vc-3. VC: MaxMatch - 랜덤하게 고른 정점에서 살아있는 간선 하나를 고른다. 양쪽 점에 대해 주위 간선을 모두 삭제한다

--- src/ch8_2_vertex_cover.py
+++ src/ch8_2_vertex_cover.py
@@ -1,5 +1,6 @@
 from vis import VertexCoverVisualizer as Visualizer
 from copy import deepcopy
+from random import randrange
 import data_sample_cities as dsc
 
 
@@ -58,6 +59,34 @@ class VertexCover:
     print(self.adjs)        # 각 점에서 연결되는 점들을 저장하는 Adj Set 을 만들어 둔다
     self.vc = set()         # 결과가 저장될 Vertex Cover
     edge_count = 0          # 지운 edge 수. n_edges 까지 지워지면 (cover 되면) 프로그램 종료
+
+    vertices = list(range(n_cities))
+    while edge_count < n_edges:
+      print(f'{self.adjs=}, {vertices=}')
+      vi = randrange(len(vertices))         # 살아있는 정점 중 하나를 랜덤하게 골라서
+      u = vertices.pop(vi)                  # 뽑아낸다
+      print(f'{vi=} {u=} {self.adjs[u]=}')
+      if not self.adjs[u]: continue         # 이 점 주위에 연결된것이 없다면 (모두 지워졌다면) 넘어간다
+      v = self.adjs[u].pop()                # 있으니까 그 중 하나를 뽑아낸다
+      print(f'{u=} {v=}')
+      vis.matching(u,v)                     # u~v 를 매칭으로 삼는다
+
+      for n in (u, v):                      # u 와 v 에 대해서 동일한 작업을 할 예정이므로 loop 를 돈다
+        self.vc.add(n)                      # 매칭의 양쪽끝점인 u 와 v 를 VC 에 추가한다
+        print(f'{self.vc=}')
+
+        for k in range(n_cities):           # 모든 점 k 에 대하여
+          if k in self.adjs[n]:             # n 에서 k 로 가는 선이 살아있다면 (안 지워졌다면)
+            print(f'<{n=} {k=} {self.adjs[n]=} {self.adjs[k]=}')
+            self.adjs[n].remove(k)          # 그 선은 지운다
+            if n in self.adjs[k]:           # k 에서 n 으로 가는 선도 살아있다면 삭제한다.
+              self.adjs[k].remove(n)        # u~v 선은 중복하여 삭제될 수 있기 때문에 살아있는지 확인 후 삭제한다
+            print(f'>{n=} {k=} {self.adjs[n]=} {self.adjs[k]=}')
+            edge_count += 1                 # 삭제한 edge 의 수를 증가시킨다
+
+      print(f'vc={self.vc} {edge_count=} {n_edges=}')
+
+      break # 일단 한바퀴만
 
     vis.draw()
 
