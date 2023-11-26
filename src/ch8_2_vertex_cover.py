14dcf6d6d538b4479e9672d4082d9fcae5e1853d
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-11-26 16:06:44 +0900

    ch82vc-1. VC: SetCover - 정점 선택시 cover 되는 간선들의 집합인 f 와 전체집합 u 를 만들고 복사본 F, U 도 만든다

--- src/ch8_2_vertex_cover.py
+++ src/ch8_2_vertex_cover.py
@@ -1,4 +1,5 @@
 from vis import VertexCoverVisualizer as Visualizer
+from copy import deepcopy
 import data_sample_cities as dsc
 
 
@@ -11,6 +12,17 @@ class VertexCover:
 
   def setCoverMain(self):
     print('Using Set Cover')
+    n_cities = len(self.cities)
+    n_edges = len(self.edges)
+    self.u = { i for i in range(n_edges) }      # self.u 는 전체 간선번호 집합
+    self.f = [ set() for _ in range(n_cities) ] # self.f 는 각 vertex 가 선택되었을 때 cover 되는 간선번호 집합
+    for i in range(n_edges):                    # 모든 edge 들에 대해 간선번호 추가
+      u,v,w = self.edges[i]
+      self.f[u].add(i)                          # u 로 cover 되는 간선번호 i 추가
+      self.f[v].add(i)                          # v 로 cover 되는 간선번호 i 추가
+    print(self.u, self.f)
+    self.U = deepcopy(self.u)                   # U, F 는 원소를 없애가면서 작업할 것이므로
+    self.F = deepcopy(self.f)                   # u, f 로부터 deepcopy 하여 준비한다
     vis.draw()
 
   def maxMatchMain(self):
