a969f4fa08dab4e4d3ef0f6c420a994e3e70308b
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-11-26 18:09:19 +0900

    ch84cl-2. Center 가 결정되었으므로 거리를 갱신한다. 처음에는 정보가 없으므로 모두 기록된다

--- src/ch8_4_cluster.py
+++ src/ch8_4_cluster.py
@@ -1,5 +1,6 @@
 from random import seed, randint
 from heapdict import heapdict
+from math import sqrt
 from data_city import five_letter_cities, City
 from vis import ClusterVisualizer as Visualizer
 
@@ -21,7 +22,26 @@ class Cluster:
     self.dists[this_center] = (0, this_center)
     self.centers.append(this_center)
 
+    # 모든 점에 대해 거리갱신을 (필요하면) 합니다
+    for i in range(n_cities):
+      # 방금 추가된 센터까지의 거리를 구해서
+      d = self.distance_between(this_center, i)
+      # dists 딕셔너리에 갱신해준다
+      if not i in self.dists or d < -self.dists[i][0]:
+        self.dists[i] = (-d, this_center)
+
+      # 각 점들에 대해 갱신하는 과정을 보여주자
+      vis.compare(i, this_center,
+        d if this_center != self.dists[i][1] else 0)
+
     vis.draw()
+
+  # i1 번째 도시와 i2 번째 도시 사이의 거리를 구한다
+  def distance_between(self, i1, i2):
+    if i1 >= len(self.cities) or i2 >= len(self.cities):
+      print(f'{i1=} {i2=} {len(self.cities)=}')
+    c1, c2 = self.cities[i1], self.cities[i2]
+    return sqrt((c1.x-c2.x)**2+(c1.y-c2.y)**2)
 
   # cities 만 남겨두고 재시작한다. 시작 도시를 랜덤하게 선택하기 때문에 다른 답을 구해 본다
   def reset(self):
